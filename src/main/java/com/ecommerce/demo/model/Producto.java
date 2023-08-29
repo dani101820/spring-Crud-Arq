e807a13e977c6501fda3b0a7199ef336e4de8ca0
unknown <daolano58@ucatolica.edu.co>
2023-08-28 21:07:33 -0500

    cambios de dos espacios que faltaban

--- src/main/java/com/ecommerce/demo/model/Producto.java
+++ src/main/java/com/ecommerce/demo/model/Producto.java
@@ -1,6 +1,5 @@
 package com.ecommerce.demo.model;
 
-
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -8,9 +7,9 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Entity;
 
 @Entity
-@Table (name = "producto")
+@Table(name = "producto")
 public class Producto {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
@@ -18,52 +17,79 @@ public class Producto {
 	private String nombreDelProducto;
 	private String descripcionDelProducto;
 	private String marcaArtesania;
-	
+	private Long unidadesDisponibles;
+	private String imagenProducto;
+
 	public Producto() {
-    }
+	}
 
 	public Producto(Long id, Long codigoEan, String nombreDelProducto, String descripcionDelProducto,
-			String marcaArtesania) {
+			String marcaArtesania, Long unidadesDisponibles, String imagenProducto) {
 		super();
 		this.id = id;
 		this.codigoEan = codigoEan;
 		this.nombreDelProducto = nombreDelProducto;
 		this.descripcionDelProducto = descripcionDelProducto;
 		this.marcaArtesania = marcaArtesania;
+		this.unidadesDisponibles = unidadesDisponibles;
+		this.imagenProducto = imagenProducto;
+
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public Long getCodigoEan() {
 		return codigoEan;
 	}
+
 	public void setCodigoEan(Long codigoEan) {
 		this.codigoEan = codigoEan;
 	}
+
 	public String getNombreDelProducto() {
 		return nombreDelProducto;
 	}
+
 	public void setNombreDelProducto(String nombreDelProducto) {
 		this.nombreDelProducto = nombreDelProducto;
 	}
+
 	public String getDescripcionDelProducto() {
 		return descripcionDelProducto;
 	}
+
 	public void setDescripcionDelProducto(String descripcionDelProducto) {
 		this.descripcionDelProducto = descripcionDelProducto;
 	}
+
 	public String getMarcaArtesania() {
 		return marcaArtesania;
 	}
+
 	public void setMarcaArtesania(String marcaArtesania) {
 		this.marcaArtesania = marcaArtesania;
 	}
-	
-	
-	
 
-	
+	public void setUnidadesDisponibles(Long unidadesDisponibles) {
+		this.unidadesDisponibles = unidadesDisponibles;
+	}
+
+	public Long getUnidadesDisponibles() {
+		return unidadesDisponibles;
+	}
+
+	public void setImagenProducto(String imagenProducto) {
+		this.imagenProducto = imagenProducto;
+	}
+
+	public String getImagenProducto() {
+		return imagenProducto;
+	}
+
 }
